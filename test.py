701508e87d797631e407ca24d8d339d11915cdbe
Gene-Weaver <willwe@umich.edu>
2023-03-31 20:24:18 -0400

    demo

---
+++ test.py
@@ -0,0 +1,22 @@
+import os
+from leafmachine2.machine.machine import machine
+from leafmachine2.machine.general_utils import load_config_file_testing
+
+def test_LM2():
+    is_test = True
+
+    # Set LeafMachine2 dir 
+    dir_home = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+    if is_test:
+        cfg_file_path = os.path.join(dir_home, 'demo','demo.yaml') #'D:\Dropbox\LeafMachine2\LeafMachine2.yaml'
+        # cfg_file_path = 'test_installation'
+
+        cfg_testing = load_config_file_testing(dir_home, cfg_file_path)
+        cfg_testing['leafmachine']['project']['dir_images_local'] = os.path.join(dir_home, cfg_testing['leafmachine']['project']['dir_images_local'][0], cfg_testing['leafmachine']['project']['dir_images_local'][1])
+        cfg_testing['leafmachine']['project']['dir_output'] = os.path.join(dir_home, cfg_testing['leafmachine']['project']['dir_output'][0], cfg_testing['leafmachine']['project']['dir_output'][1])
+
+        machine(cfg_file_path, dir_home, cfg_testing)
+
+if __name__ == '__main__':
+    test_LM2()
