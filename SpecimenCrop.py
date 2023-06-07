fad5ee6e38a1261846ecc3819670819e1d604241
Gene-Weaver <willwe@umich.edu>
2023-06-07 15:44:50 -0400

    demo.yaml

---
+++ SpecimenCrop.py
@@ -0,0 +1,9 @@
+from leafmachine2.machine.specimencrop import machine
+import os
+if __name__ == '__main__':
+    # To use LeafMachine2.yaml file, set cfg_file_path = None
+    # To switch between config files, you can provide the full path to a different config file
+    cfg_file_path = None
+    cfg_testing = None
+    dir_home = os.path.dirname(__file__)
+    machine(cfg_file_path, dir_home, cfg_testing)
