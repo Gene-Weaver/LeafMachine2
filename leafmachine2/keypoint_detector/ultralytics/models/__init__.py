a3ff6ea607d44177a169001c2ebfd42983340a49
Gene-Weaver <willwe@umich.edu>
2024-08-14 17:31:15 -0400

    add keypoint detector and newest segmentation

--- leafmachine2/keypoint_detector/ultralytics/models/__init__.py
+++ leafmachine2/keypoint_detector/ultralytics/models/__init__.py
@@ -1,5 +1,17 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+parentdir1 = os.path.dirname(os.path.dirname(currentdir))
+parentdir2 = os.path.dirname(os.path.dirname(os.path.dirname(currentdir)))
+parentdir3 = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(currentdir))))
+sys.path.append(currentdir)
+sys.path.append(parentdir1)
+sys.path.append(parentdir2)
+sys.path.append(parentdir3)
+
 from .rtdetr import RTDETR
 from .sam import SAM
 from .yolo import YOLO
