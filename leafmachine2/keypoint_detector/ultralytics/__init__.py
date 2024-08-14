d5fb3e9b5d7a8bb3718f478dd7f5aed3d4aba96d
Gene-Weaver <willwe@umich.edu>
2024-08-14 17:22:43 -0400

    add keypoint detector and newest segmentation

--- leafmachine2/keypoint_detector/ultralytics/__init__.py
+++ leafmachine2/keypoint_detector/ultralytics/__init__.py
@@ -13,11 +13,19 @@ sys.path.append(currentdir)
 sys.path.append(parentdir1)
 sys.path.append(parentdir2)
 
-from ultralytics.models import RTDETR, SAM, YOLO
-from ultralytics.models.fastsam import FastSAM
-from ultralytics.models.nas import NAS
-from ultralytics.utils import SETTINGS as settings
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
+try:
+    from ultralytics.models import RTDETR, SAM, YOLO
+    from ultralytics.models.fastsam import FastSAM
+    from ultralytics.models.nas import NAS
+    from ultralytics.utils import SETTINGS as settings
+    from ultralytics.utils.checks import check_yolo as checks
+    from ultralytics.utils.downloads import download
+except:
+    from leafmachine2.keypoint_detector.ultralytics.models import RTDETR, SAM, YOLO
+    from leafmachine2.keypoint_detector.ultralytics.models.fastsam import FastSAM
+    from leafmachine2.keypoint_detector.ultralytics.models.nas import NAS
+    from leafmachine2.keypoint_detector.ultralytics.utils import SETTINGS as settings
+    from leafmachine2.keypoint_detector.ultralytics.utils.checks import check_yolo as checks
+    from leafmachine2.keypoint_detector.ultralytics.utils.downloads import download
 
 __all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'
